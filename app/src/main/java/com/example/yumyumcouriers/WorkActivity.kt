cdf81323fc16f37360f5e7b5091701801a409576
ярослав Колпаков <kolpackov.yarosl@yandex.ru>
2024-04-14 23:10:42 +0700

    Add Marker

--- app/src/main/java/com/example/yumyumcouriers/WorkActivity.kt
+++ app/src/main/java/com/example/yumyumcouriers/WorkActivity.kt
@@ -3,16 +3,22 @@ package com.example.yumyumcouriers
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.yumyumcouriers.R
 import com.yandex.mapkit.MapKitFactory
-import com.yandex.mapkit.geometry.Point as MapPoint
+import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.map.PlacemarkMapObject
+import com.yandex.runtime.image.ImageProvider
+import com.yandex.mapkit.map.MapObjectCollection
+
+
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class WorkActivity : AppCompatActivity(){
     private lateinit var mapView: MapView
     private lateinit var btnShowBottomSheet: Button
+    private lateinit var mapObjectCollection: MapObjectCollection
+    private lateinit var placemarkMapObject: PlacemarkMapObject
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +30,13 @@ class WorkActivity : AppCompatActivity(){
         mapView = findViewById(R.id.mapview)
         mapView.map.move(
             CameraPosition(
-                MapPoint(55.031091, 82.920675),
+                Point(55.031091, 82.920675),
                 /* zoom = */ 17.0f,
                 /* azimuth = */ 0.0f,
                 /* tilt = */ 30.0f
             ))
+        setMarkerInStartLocation()
+
 
         // Инициализация кнопки для отображения Bottom Sheet Dialog
         btnShowBottomSheet = findViewById(R.id.idBtnShowBottomSheet)
@@ -47,9 +55,12 @@ class WorkActivity : AppCompatActivity(){
             dialog.setContentView(view)
             dialog.show()
         }
-
-        // Запуск построения маршрута
-        val start = MapPoint(55.031091, 82.920675) // Начальная точка
-        val end = MapPoint(55.041091, 82.920675) // Конечная точка
+    }
+    private fun setMarkerInStartLocation() {
+        val marker = R.drawable.back // Добавляем ссылку на картинку
+        mapObjectCollection = mapView.map.mapObjects // Инициализируем коллекцию различных объектов на карте
+        placemarkMapObject = mapObjectCollection.addPlacemark(Point(55.031091, 82.920675), ImageProvider.fromResource(this, marker)) // Добавляем метку со значком
+        placemarkMapObject.opacity = 0.5f // Устанавливаем прозрачность метке
+        placemarkMapObject.setText("Обязательно к посещению!") // Устанавливаем текст сверху метки
     }
 }
